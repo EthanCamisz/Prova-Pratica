229c56dd75e59657298e9d2f5bdced6de0f9967e
EthanCamisz <110117563+EthanCamisz@users.noreply.github.com>
2023-07-10 18:22:18 -0300

    Add files via upload

---
+++ Prova/index.js
@@ -0,0 +1,43 @@
+//1) criando um servidor 
+//const http = require ('http')
+//const fs = require('fs')
+//const port = 3203
+//const os = require('os')
+
+// nome do sistema
+//console.log(os.type())
+
+//memória disponível no servidor
+//console.log(os.freemem())
+//var memo = os.freemem / 1000
+//console.log(`${memo} KB`)
+
+//Diretório de Trabalho atual do servidor
+//console.log(os.homedir())
+
+//const server = http.createServer((req, res) => {
+    //res.writeHead(200, { 'Content-Type': 'application/json' });
+    //res.end('okay');
+    //res.write(data)
+    //return res.end()
+//})
+//server.listen(port, ()=>{
+    //console.log(`Servidor On! ${port} (o゜▽゜)o☆`)
+
+//})
+
+ 
+
+// 4) renomeando um arquivo:
+//const fs = require('fs')
+
+//fs.rename('Bratz.txt', 'Barbie.txt', function (err, data){
+    //if (err) throw (err)
+   // console.log(`Arquivo Renomeado! (o゜▽゜)o☆`)
+//})
+
+//5) Informações CPU:
+
+//const os = require('os')
+//console.log(os.cpus())
+
